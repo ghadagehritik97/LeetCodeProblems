e0262ba74197c8d9b27781e88118f8d9e6b14724
HRITIK GHADAGE <87740046+ghadagehritik97@users.noreply.github.com>
2024-12-10 23:36:16 +0530

    Update SplitArrayLargestSum.java

--- src/SplitArrayLargestSum.java
+++ src/SplitArrayLargestSum.java
@@ -1,5 +1,5 @@
 package src;
-
+//question https://leetcode.com/problems/split-array-largest-sum/
 public class SplitArrayLargestSum {
     public static void main(String[] args) {
         int []nums={7,2,5,10,8};
