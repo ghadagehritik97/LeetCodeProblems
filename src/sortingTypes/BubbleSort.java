48754e40ff79b4419ce0659a5ce6b0db5e9a27d9
Hritik Ghadage <ghadagehritik97@gmail.com>
2024-12-12 23:49:23 +0530

    bubble sorting code

---
+++ src/sortingTypes/BubbleSort.java
@@ -0,0 +1,29 @@
+package src.sortingTypes;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int []arr={1,2,3,4,5};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubbleSort(int[] arr) {
+
+        for(int i=0;i<arr.length;i++){
+                boolean swap=false;
+            for(int j=1;j<arr.length-i;j++){
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                    swap=true;
+                }
+            }
+            if(!swap){
+                break;
+            }
+        }
+    }
+}
