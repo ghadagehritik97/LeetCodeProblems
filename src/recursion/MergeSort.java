d555cbbc922a69040016be324b0b6c01fd8eac1c
Hritik Ghadage <ghadagehritik97@gmail.com>
2025-04-07 16:50:28 +0530

    merge sort using recursion

---
+++ src/recursion/MergeSort.java
@@ -0,0 +1,51 @@
+package src.recursion;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int []arr={5,6,4,3,2,1};
+        int []ans=mergeSort(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    private static int[] mergeSort(int[] arr) {
+        if(arr.length==1){
+            return arr;
+        }
+        int mid= arr.length/2;
+        int []left=mergeSort(Arrays.copyOfRange(arr,0,mid));
+        int []right=mergeSort(Arrays.copyOfRange(arr,mid,arr.length));
+        return mergeArrays(left,right);
+
+    }
+
+    private static int[] mergeArrays(int[] left, int[] right) {
+        int []ans=new int[left.length+right.length];
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i<left.length && j<right.length){
+            if(left[i]<right[j]){
+                ans[k]=left[i];
+                i++;
+            }
+            else{
+                ans[k]=right[j];
+                j++;
+            }
+            k++;
+        }
+        while(i< left.length){
+            ans[k]=left[i];
+            i++;
+            k++;
+        }
+        while(j<right.length){
+            ans[k]=right[j];
+            j++;
+            k++;
+        }
+        return ans;
+    }
+}
