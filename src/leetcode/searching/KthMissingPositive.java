e993ee813afd874bbb8ff33e894a99b11ceaf5f0
Hritik Ghadage <ghadagehritik97@gmail.com>
2025-05-14 22:14:52 +0530

    added question link

--- src/leetcode/searching/KthMissingPositive.java
+++ src/leetcode/searching/KthMissingPositive.java
@@ -1,5 +1,5 @@
 package src.leetcode.searching;
-
+//https://leetcode.com/problems/kth-missing-positive-number/description/
 public class KthMissingPositive {
     public static void main(String[] args) {
         int []arr={1,2,3};
